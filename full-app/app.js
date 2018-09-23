bc4daf97b5bde5d39ac01f7d53a65e93d5bfa103
Mario De Donno <mamidd@gmail.com>
2018-09-23 19:53:45 +0200

    feat: completed app

---
+++ full-app/app.js
@@ -0,0 +1,66 @@
+// console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+
+const argv = yargs
+  .command('add', 'Add a new Note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
+let command = argv._[0];
+
+// console.log('Yargs: ', argv);
+// console.log('Command: ', command);
+
+if(command === 'add'){
+  let note = notes.addNote(argv.title, argv.body);
+  if (note){
+    console.log('Note created');
+    notes.logNote(note);
+  }else{
+    console.log('Note already exists.');
+  }
+}else if(command === 'list'){
+  let allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
+}else if(command === 'read'){
+  let note = notes.getNote(argv.title);
+  if (note){
+    console.log('Note found');
+    notes.logNote(note);
+  }else{
+    console.log('Note not found.');
+  }
+}else if(command === 'remove'){
+  let noteRemoved = notes.removeNote(argv.title);
+  let message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
+}else{
+  console.log('Command not recognized');
+}
---
+++ lesson14/app.js
@@ -0,0 +1,27 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+// console.log('Process: ', process.argv);
+console.log('Yargs: ', argv);
+
+// let command = process.argv[2];
+let command = argv._[0];
+console.log('Command: ', command);
+
+if(command === 'add'){
+  notes.addNote(argv.title, argv.body);
+}else if(command === 'list'){
+  notes.getAll();
+}else if(command === 'read'){
+  notes.getNote(argv.title);
+}else if(command === 'remove'){
+  notes.removeNote(argv.title);
+}else{
+  console.log('Command not recognized');
+}
